3e533249bae6a6503c89189b08593c752e0b49e3
mengjuhua <mengjuhua@risesoft.net>
2024-12-26 10:08:35 +0800

    chore：添加gradle构建

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,17 @@ plugins {
     id("com.vanniktech.maven.publish") version "0.30.0" apply false
     id("tech.yanand.maven-central-publish") version "1.3.0" apply false
 
-    id("net.risesoft.y9.aspectj") version "0.0.5" apply false
-    id("net.risesoft.y9.docker") version "0.0.5" apply false
-    id("net.risesoft.y9.java-conventions") version "0.0.5" apply false
-    id("net.risesoft.y9.java-publish") version "0.0.5" apply false
-    id("net.risesoft.y9.java-publish-central") version "0.0.5" apply false
-    id("net.risesoft.y9.javaPlatform-publish") version "0.0.5" apply false
-    id("net.risesoft.y9.javaPlatform-publish-central") version "0.0.5" apply false
-    id("net.risesoft.y9.lombok") version "0.0.5" apply false
-    id("net.risesoft.y9.management") version "0.0.5" apply true
-    id("net.risesoft.y9.repository") version "0.0.5" apply false
-    id("net.risesoft.y9.smart-doc") version "0.0.5" apply false
+    id("net.risesoft.y9.aspectj") version "9.7.0-01" apply false
+    id("net.risesoft.y9.docker") version "9.7.0-01" apply false
+    id("net.risesoft.y9.java-conventions") version "9.7.0-01" apply false
+    id("net.risesoft.y9.java-publish") version "9.7.0-01" apply false
+    id("net.risesoft.y9.java-publish-central") version "9.7.0-01" apply false
+    id("net.risesoft.y9.javaPlatform-publish") version "9.7.0-01" apply false
+    id("net.risesoft.y9.javaPlatform-publish-central") version "9.7.0-01" apply false
+    id("net.risesoft.y9.lombok") version "9.7.0-01" apply false
+    id("net.risesoft.y9.management") version "9.7.0-01" apply true
+    id("net.risesoft.y9.repository") version "9.7.0-01" apply false
+    id("net.risesoft.y9.smart-doc") version "9.7.0-01" apply false
 }
 
 repositories {
