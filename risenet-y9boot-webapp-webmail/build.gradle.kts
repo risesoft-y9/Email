ebd4aa891ef8e4f55d652b94835fc60b78c59931
mengjuhua <mengjuhua@risesoft.net>
2025-01-22 10:25:53 +0800

    chore：升级y9-plugin版本-》9.7.0-08

--- risenet-y9boot-webapp-webmail/build.gradle.kts
+++ risenet-y9boot-webapp-webmail/build.gradle.kts
@@ -2,26 +2,27 @@ plugins {
     alias(libs.plugins.y9.docker)
     alias(libs.plugins.y9.conventions.war)
     alias(libs.plugins.y9.lombok)
-    alias(y9libs.plugins.org.springframework.boot)
 }
 
 dependencies {
-    // api(platform(libs.y9.digitalbase.bom))
-    providedRuntime(platform(y9libs.spring.boot.bom))
+    providedRuntime(platform(libs.y9.digitalbase.dependencies))
 
     api(project(":risenet-y9boot-support-webmail"))
     api("net.risesoft:risenet-y9boot-starter-sso-oauth2-resource")
     api("net.risesoft:risenet-y9boot-common-nacos")
     api("org.apache.commons:commons-pool2")
 
+    compileOnly("jakarta.servlet:jakarta.servlet-api")
     providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
 }
 
 description = "risenet-y9boot-webapp-webmail"
 
 val finalName = "webmail"
-jib.container.appRoot = "/usr/local/tomcat/webapps/${finalName}"
+y9Docker {
+    appName = finalName
+}
 
-tasks.bootWar {
-    archiveFileName.set("${finalName}.${archiveExtension.get()}")
+y9War {
+    archiveBaseName = finalName
 }
