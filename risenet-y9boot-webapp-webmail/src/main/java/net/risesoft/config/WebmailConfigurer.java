9d93c8ab148ae9d1797ccbe0ca958d0ba45d4a2f
shidaobang <daobangshi@gmail.com>
2025-02-06 10:50:32 +0800

    refactor: 移除无用的配置
    (cherry picked from commit 16fb5cdbdac3d11d70b1d21106975b79655c6d19)

--- risenet-y9boot-webapp-webmail/src/main/java/net/risesoft/config/WebmailConfigurer.java
+++ risenet-y9boot-webapp-webmail/src/main/java/net/risesoft/config/WebmailConfigurer.java
@@ -1,13 +1,8 @@
 package net.risesoft.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,15 +13,6 @@ import net.risesoft.y9.configuration.Y9Properties;
 @EnableScheduling
 @EnableConfigurationProperties(Y9Properties.class)
 public class WebmailConfigurer implements WebMvcConfigurer {
-
-    @Bean
-    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        List<MediaType> supportedMediaTypes = new ArrayList<MediaType>();
-        supportedMediaTypes.add(MediaType.parseMediaType("text/html;charset=UTF-8"));
-        converter.setSupportedMediaTypes(supportedMediaTypes);
-        return converter;
-    }
 
     @Bean
     public Y9Context y9Context() {
