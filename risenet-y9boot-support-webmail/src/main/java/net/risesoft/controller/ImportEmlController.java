f5e33056715cb6386883dc1e16419f39d756a12b
mengjuhua <mengjuhua@risesoft.net>
2026-03-03 15:51:42 +0800

    fix: List参数使用：NotEmpty判空
    (cherry picked from commit e03d92e672d95370acb8f298077ed1445e8c8f84)

--- risenet-y9boot-support-webmail/src/main/java/net/risesoft/controller/ImportEmlController.java
+++ risenet-y9boot-support-webmail/src/main/java/net/risesoft/controller/ImportEmlController.java
@@ -13,6 +13,7 @@ import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 
 import org.apache.james.mime4j.dom.Message;
 import org.apache.james.mime4j.stream.MimeConfig;
@@ -97,7 +98,7 @@ public class ImportEmlController {
 
     @RiseLog(operationName = "批量删除导入信息")
     @PostMapping(value = "/deleteEml")
-    public Y9Result<Object> deleteEml(@RequestParam @NotBlank List<String> ids) {
+    public Y9Result<Object> deleteEml(@RequestParam @NotEmpty List<String> ids) {
         importEmlService.delete(ids);
         return Y9Result.successMsg("删除邮件EML详细信息成功");
     }
