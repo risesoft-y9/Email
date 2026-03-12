baf6878752d812fea87cf27aaecbfad5397a372f
shidaobang <shidaobang@risesoft.net>
2026-03-12 15:54:31 +0800

    feat: 自定义 springboot 应用的 banner，打印一些有用信息

--- risenet-y9boot-server-webmail/src/main/java/net/risesoft/ServletInitializer.java
+++ risenet-y9boot-server-webmail/src/main/java/net/risesoft/ServletInitializer.java
@@ -3,12 +3,14 @@ package net.risesoft;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import net.risesoft.y9.spring.boot.Y9Banner;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
         setRegisterErrorPageFilter(false);
-        builder.sources(WebmailApplication.class);
+        builder.sources(WebmailApplication.class).banner(new Y9Banner());
         return builder;
     }
 }
--- risenet-y9boot-server-webmail/src/main/java/net/risesoft/WebmailApplication.java
+++ risenet-y9boot-server-webmail/src/main/java/net/risesoft/WebmailApplication.java
@@ -3,10 +3,14 @@ package net.risesoft;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import net.risesoft.y9.spring.boot.Y9Banner;
+
 @SpringBootApplication
 public class WebmailApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebmailApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(WebmailApplication.class);
+        springApplication.setBanner(new Y9Banner());
+        springApplication.run(args);
     }
 }
